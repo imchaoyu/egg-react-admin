ac9ec6e3a290f0e334320234f0207aedda285efc
chaoyu <chaoyumail@126.com>
2022-05-09 17:06:48 +0800

    代码格式优化

---
+++ admin/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  globals: {
+    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
+    page: true,
+    REACT_APP_ENV: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'error',
+    'space-before-function-paren': 'off',
+  },
+};
