f751a4a7d4042d3ede408c677347119d0bf6122d
chaoyu <chaoyumail@126.com>
2022-05-09 21:59:04 +0800

    html模板和登录空页面

--- admin/config/routes.js
+++ admin/config/routes.js
@@ -1,4 +1,18 @@
 export default [
+  {
+    path: '/login',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/login',
+        component: '@/pages/login/login',
+      },
+      {
+        component: './errorPages/404',
+      },
+    ],
+  },
   { path: '/', component: '@/pages/index' },
   {
     component: './errorPages/404',
