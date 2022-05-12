668ff6696d2fe9102897fbac4a59bb7e99de4721
chaoyu <chaoyumail@126.com>
2022-05-12 16:33:29 +0800

    stylelint降级

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,4 @@
 module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/stylelint')],
-  rules: {
-    'no-invalid-double-slash-comments': null,
-    'at-rule-no-unknown': null,
-  },
+  rules: {},
 };
