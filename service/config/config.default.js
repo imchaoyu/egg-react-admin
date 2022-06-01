fb054464c40e880d15d5d174e1c2605866a27bd6
chaoyu <chaoyumail@126.com>
2022-06-01 16:22:22 +0800

    统一错误处理

--- service/config/config.default.js
+++ service/config/config.default.js
@@ -16,7 +16,7 @@ module.exports = (appInfo) => {
   config.keys = appInfo.name + '_1653984245142_8524';
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = ['errorHandler'];
 
   // add your user config here
   const userConfig = {
