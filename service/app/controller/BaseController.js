3e6eda325c3d6beaa49a9b4830d1205af19f75fc
chaoyu <chaoyumail@126.com>
2022-06-02 16:52:26 +0800

    BaseController优化

--- service/app/controller/BaseController.js
+++ service/app/controller/BaseController.js
@@ -3,15 +3,30 @@
 const { Controller } = require('egg');
 
 class BaseController extends Controller {
-  get user() {
-    return this.ctx.session.user;
-  }
-
-  success(data) {
-    this.ctx.body = {
-      success: true,
+  async success({ data = null, msg = '操作成功' } = {}) {
+    const { ctx } = this;
+    ctx.body = {
+      errCode: 200,
+      msg,
       data,
     };
+    ctx.status = 200;
+  }
+  // 请求失败操作
+  async fail(err) {
+    const { ctx } = this;
+    // 验证字段失败 返回422
+    const errText = err.status !== 422 ? err.message : err.errors[0].message;
+    if (err) {
+      ctx.body = {
+        errCode: err.status || 500,
+        msg: errText,
+        data: null,
+      };
+      ctx.status = 200;
+    } else {
+      throw new Error(err || '未知错误');
+    }
   }
 
   notFound(msg) {
