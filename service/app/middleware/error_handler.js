cdec9c549595753562f892118d5e417d90b1d284
chaoyu <chaoyumail@126.com>
2022-06-07 11:23:36 +0800

    修改统一错误返回信息

--- service/app/middleware/error_handler.js
+++ service/app/middleware/error_handler.js
@@ -10,15 +10,15 @@ module.exports = () => {
 
       const status = err?.status ?? 500;
       // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
-      const error =
+      const msg =
         status === 500 && ctx?.app?.config?.env === 'prod' ? 'Internal Server Error' : err.message;
 
       // 从 error 对象上读出各个属性，设置到响应中
-      ctx.body = { error };
-      if (status === 422) {
-        ctx.body.detail = err.errors;
-      }
-      ctx.status = status;
+      ctx.body = {
+        errCode: status,
+        msg,
+      };
+      ctx.status = 200;
     }
   };
 };
