dc94120d4ff1cc48d78e480963848a5d552a2491
chaoyu <chaoyumail@126.com>
2022-06-22 15:49:44 +0800

    🏰 chore: ignore文件修改

---
+++ admin/config/config.settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  // 密码
+  DEFAULT_KEY: 'i9Mey6cPhY/qB6jhbm5VdPdWG-default_',
+  SESSION_KEY: 'W8x+Z718eHH40lLD+KC2uKg+EU8@SESSION',
+  IV: 'mavT+QJEwJDamiX@iv',
+};
+export default settings;
